8ba6dc5b34c23500831e4d4da7eaeab401996495
dev-rome <info.devrome@gmail.com>
2025-12-19 21:09:17 -0500

    refactor: remove unused import from home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,11 @@ import { Methodology } from "@/sections/MethodologySection/Methodology";
 import { Projects } from "@/sections/ProjectsSection/Projects";
 import { Testimonials } from "@/sections/TestimonialsSection/Testimonials";
 import { Contact } from "@/sections/ContactSection/Contact";
-import { FloatingControls } from "@/components/FloatingControls";
 import styles from "./Layout.module.css";
 
 export default function Home() {
   return (
     <main className={styles.pageMain}>
-      <FloatingControls />
       <Hero />
       <About />
       <Methodology />
